5ecd138751b0b3b356841a366247dbce85cb31b5
sonmh79 <sonmh79@naver.com>
2021-03-19 21:03:42 +0900

    update
    make nums ascending
    make different colors by level

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
 const btn = document.querySelector("button");
+const nums = document.querySelectorAll(".num");
 
-
+function setNumbers(){
+    
+}
 
 function makeNum(e){
+    nums.forEach(function(num){
+        num.classList.remove(num.classList[1])
+    })
     let luckyNums = [];
-    while (luckyNums.length !== 7) {
+    while (luckyNums.length !== 6) {
         let num = Math.floor(Math.random()*45)+1;
         if (luckyNums.includes(num)){
         }else {
             luckyNums.push(num)
         }
     }
-    const n1 = document.querySelector(".num1"),
-    n2 = document.querySelector(".num2"),
-    n3 = document.querySelector(".num3"),
-    n4 = document.querySelector(".num4"),
-    n5 = document.querySelector(".num5"),
-    n6 = document.querySelector(".num6");
-    n7 = document.querySelector(".num7");
-    n1.innerText = luckyNums[0];
-    n2.innerText = luckyNums[1];
-    n3.innerText = luckyNums[2];
-    n4.innerText = luckyNums[3];
-    n5.innerText = luckyNums[4];
-    n6.innerText = luckyNums[5];
-    n7.innerText = luckyNums[6];
+    luckyNums.sort(function(a,b){
+        while (luckyNums.length !==7) {
+            let bonus =  Math.floor(Math.random()*45)+1;
+            if (luckyNums.includes(bonus)){
+            } else {
+                luckyNums.push(bonus)
+            }
+        }
+        return a - b;
+    })
+    
+    for (var i = 0;i<7;i++){
+        let num = document.querySelector(`.num${i+1}`);
+        num.innerText = luckyNums[i]
+        if (luckyNums[i]<11){
+            num.parentNode.classList.add("yellow");
+        } else if (luckyNums[i]<21){
+            num.parentNode.classList.add("blue");
+        } else if (luckyNums[i]<31){
+            num.parentNode.classList.add("pink");
+        } else if (luckyNums[i]<41){
+            num.parentNode.classList.add("purple");
+        } else{
+            num.parentNode.classList.add("green");
+        }
+    }
 }
 function init(){
     btn.addEventListener("click",makeNum)
-    console.log([7,8,9].includes(7))
 }
 init()
